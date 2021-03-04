52bb5501d77e741bef5e6fd493648249e9c66a13
Luiz Henrique <lmenezesmessias@gmail.com>
2021-03-03 21:22:09 -0300

    Final

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Atividade_AsciiArt_PC1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("    ||  ||  ");
+            Console.WriteLine("    \\\\()//  ");
+            Console.WriteLine("   //(__)\\\\ ");
+            Console.WriteLine("   ||    || ");
+
+        }
+    }
+}
